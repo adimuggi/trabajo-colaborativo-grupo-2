817b579380a4ef0f8ab77cbdd43c369324647b98
aldair <adimuggi@gmail.com>
2025-12-09 13:26:32 -0500

    primer cambio

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Sobre Nosotros</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a class="active" href="about.php">Sobre Nosotros</a></li>
+            <li><a href="contact.php">Contacto</a></li>
+            <li><a href="register.php">Registro</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <h1>¿Quiénes somos?</h1>
+    <p>Somos un equipo de estudiantes desarrollando una página web de forma colaborativa.  
+       Nuestro objetivo es aprender GitHub, organización de proyectos y trabajo en equipo.</p>
+</main>
+
+<footer>
+    <p>© 2025 Proyecto Web Colaborativo</p>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio - Proyecto Web</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+<header>
+    <img src="img/banner.jpg" class="banner">
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="about.php">Sobre Nosotros</a></li>
+            <li><a href="contact.php">Contacto</a></li>
+            <li><a href="register.php">Registro</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <h1>Bienvenido a nuestro Proyecto Web Colaborativo</h1>
+    <p>Este sitio fue desarrollado por el grupo como parte de un trabajo colaborativo usando GitHub.</p>
+</main>
+
+<footer>
+    <p>© 2025 Proyecto Web Colaborativo</p>
+</footer>
+
+<script src="js/funciones.js"></script>
+</body>
+</html>
